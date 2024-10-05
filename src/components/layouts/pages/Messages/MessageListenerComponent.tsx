26161efa7da94ed9ecdf354e9bee0d782f5fdff0
farrar142 <gksdjf1690@gmail.com>
2024-10-05 16:10:13 +0900

    fix:메세지 웹소켓을 유저용 제네릭 웹소켓으로 변경

--- src/components/layouts/pages/Messages/MessageListenerComponent.tsx
+++ src/components/layouts/pages/Messages/MessageListenerComponent.tsx
@@ -6,6 +6,15 @@ import { useInComingMessages, useMessageGroupList } from './MessageGroupAtom';
 import API from '#/api';
 import useUser from '#/hooks/useUser';
 
+type WSMessageEvent = {
+  type: 'message';
+  message: Message;
+};
+type WSNotificationEvent = {
+  type: 'notification';
+  notification: number;
+};
+type WSEvent = WSMessageEvent | WSNotificationEvent;
 export const MessageListenerComponent: React.FC = () => {
   const [user] = useUser();
   const [_, setInComingMessage] = useInComingMessages();
@@ -13,23 +22,25 @@ export const MessageListenerComponent: React.FC = () => {
   const { groupList, handleGroupList, groupListRef } = useMessageGroupList();
   useEffect(() => {
     if (!user) return;
-    const ws = new WS<Message>(`/ws/message_users/${user.id}/`);
+    const ws = new WS<WSEvent>(`/ws/users/${user.id}/`);
     ws.onopen = () => {};
-    ws.parseMessage((message) => {
-      console.log('on messages', message);
-      const flattened = groupListRef.current;
-      const matched = flattened.filter((group) => group.id === message.group);
-      if (matched.length === 0) {
-        console.log('not matched');
-        API.Messages.message
-          .getItem(message.group)
-          .then((r) => r.data)
-          .then((group) => {
-            if (!group.latest_message) return;
-            handleGroupList([{ ...group }]);
-          });
+    ws.parseMessage((event) => {
+      if (event.type === 'message') {
+        const message = event.message;
+        const flattened = groupListRef.current;
+        const matched = flattened.filter((group) => group.id === message.group);
+        if (matched.length === 0) {
+          console.log('not matched');
+          API.Messages.message
+            .getItem(message.group)
+            .then((r) => r.data)
+            .then((group) => {
+              if (!group.latest_message) return;
+              handleGroupList([{ ...group }]);
+            });
+        }
+        setInComingMessage((p) => [...p, message]);
       }
-      setInComingMessage((p) => [...p, message]);
     });
     return () => ws.close();
   }, [user?.id]);
