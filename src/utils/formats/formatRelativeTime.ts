3f210e6d272057de4ed339958ef3fc807b536fb7
farrar142 <gksdjf1690@gmail.com>
2024-10-09 12:53:21 +0900

    fix:날짜포맷추가

--- src/utils/formats/formatRelativeTime.ts
+++ src/utils/formats/formatRelativeTime.ts
@@ -9,7 +9,6 @@ export const formatRelativeTime = (inputTime: string) => {
 
   // 두 시간의 차이를 계산
   const duration = moment.duration(now.diff(givenTime));
-
   // 차이에 따른 포맷 결정
   if (duration.asSeconds() < 60) {
     return `${Math.floor(duration.asSeconds())} seconds ago`;
@@ -17,7 +16,11 @@ export const formatRelativeTime = (inputTime: string) => {
     return `${Math.floor(duration.asMinutes())} minutes ago`;
   } else if (duration.asHours() < 24) {
     return `${Math.floor(duration.asHours())} hours ago`;
-  } else {
+  } else if (duration.asMonths() < 1) {
     return `${Math.floor(duration.asDays())} days ago`;
+  } else if (duration.asYears() < 1) {
+    return `${Math.floor(duration.months())} months ago`;
+  } else {
+    return `${Math.floor(duration.years())} years ago`;
   }
 };
