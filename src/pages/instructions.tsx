717a608e4e1173281edce5a7f56aeb3b10565ec1
farrar142 <gksdjf1690@gmail.com>
2024-10-14 15:42:02 +0900

    fix:instruction 페이지에 사용자가 로그인시 알람

--- src/pages/instructions.tsx
+++ src/pages/instructions.tsx
@@ -2,6 +2,8 @@ import 'react-notion/src/styles.css';
 import { NotionRenderer } from 'react-notion';
 import { Box } from '@mui/material';
 import { useDarkMode } from '#/styles';
+import API from '#/api';
+import axios from 'axios';
 const InstructionPage: ExtendedNextPage<{ blockMap: {} }> = ({ blockMap }) => {
   const [isDark] = useDarkMode();
   return (
@@ -25,12 +27,28 @@ const InstructionPage: ExtendedNextPage<{ blockMap: {} }> = ({ blockMap }) => {
   );
 };
 
-InstructionPage.getInitialProps = async () => {
+InstructionPage.getInitialProps = async (ctx) => {
+  const TEST_USER_EMAIL = process.env.TEST_USER_EMAIL;
+  const TEST_USER_PASSWORD = process.env.TEST_USER_PASSWORD;
+  const WEBHOOK = process.env.DISCORD_WEBHOOK;
+  let tokens = undefined;
+  if (TEST_USER_EMAIL && TEST_USER_PASSWORD) {
+    tokens = await API.Auth.signin({
+      email: TEST_USER_EMAIL,
+      password: TEST_USER_PASSWORD,
+    }).then((r) => r.data);
+    if (WEBHOOK) {
+      axios.post(WEBHOOK, {
+        content: 'User Login',
+      });
+    }
+  }
   const data = await fetch(
     `https://notion-api.splitbee.io/v1/page/11d14ff5734480709083d5004e3e3062`
   ).then((d) => d.json());
   return {
     blockMap: data,
+    tokens,
   };
 };
 
